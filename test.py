52b5ab34b3d305510986731cb2bfad67cdd3fef5
Hea092024 <Hea092024@gmail.com>
2025-01-15 12:12:20 +0100

    updated test.py

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ num = 2
 def summer(num1, num2):
     print( num1 + num2)
 
+def Num2Creator(num1):
+    print("give second number")
+    NummerTo = decimal.Decimal(input())
+    summer(num1, NummerTo)
+
+
+def num1Creator(): 
+    print("give first number")
+    NummerEn = decimal.Decimal(input())
+    Num2Creator(NummerEn)
